cbbc2e05a54516274bed94b8384417c340fabf1d
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-25 06:26:48 +0000

    Working lat and lng print

---
+++ umbrella.rb
@@ -0,0 +1,30 @@
+pp "Hi"
+
+pp "Where are you located?"
+
+user_location = gets.chomp.gsub(" ", "%20")
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+require "http"
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitude = loc.fetch("lat")
+pp longitude = loc.fetch("lng")
